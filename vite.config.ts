00353c7bf85afde7e9c7848f4f92d14f0059e902
thiagokassabian <thiago.kassabian@gmail.com>
2026-03-20 19:20:11 -0300

    fix deploy pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,18 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+	server: {
+		host: "::",
+		port: 8080,
+		hmr: {
+			overlay: false
+		}
+	},
+	plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src")
+		}
+	},
+	base: "/distritoinovacao/"
+}))
